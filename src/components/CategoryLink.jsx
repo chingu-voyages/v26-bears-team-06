8a51d1c27054e832042225111562e941b0a7205c
Marie Otaki <otakimarie@gmail.com>
2021-01-17 09:39:38 -0800

    add category links

--- src/components/CategoryLink.jsx
+++ src/components/CategoryLink.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles, Grid } from '@material-ui/core';
 
-const CategoryLink = ({ category }) => {
-  return <Link>{category}</Link>;
+const CategoryLink = ({ category, imageURL }) => {
+  const useStyles = makeStyles({
+    categoryLink: {
+      backgroundColor: 'red',
+      border: '1px solid #757373',
+      display: 'grid',
+      alignContent: 'center',
+      justifyContent: 'center',
+      backgroundImage: `url(${imageURL})`,
+      backgroundSize: 'cover',
+      textDecoration: 'none',
+    },
+    categorTitle: {
+      fontSize: '1.5em',
+      fontWeight: 'bold',
+      color: '#000',
+    },
+  });
+  const classes = useStyles();
+  console.log(imageURL);
+  return (
+    <Link className={classes.categoryLink}>
+      <p className={classes.categoryLink}>{category}</p>
+    </Link>
+  );
 };
 
 export default CategoryLink;
--- src/components/CategoryListContainer.jsx
+++ src/components/CategoryListContainer.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import Headline2 from './Headline2';
 import CategoryLink from './CategoryLink';
+import { makeStyles, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  categoryContainer: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateRows: '320px',
+    gridTemplateAreas: 'category1 category2 category3',
+  },
+});
 
 const CategoryListContainer = () => {
+  const classes = useStyles();
+  const communityImageURL =
+    'https://images.unsplash.com/photo-1550096141-7263640aa48c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
+  const saleImageURL =
+    'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
+
+  const jobsImageURL =
+    'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
+
   return (
     <div>
       <Headline2 headline="San Francisco, CA" />
-      <div>
-        <CategoryLink category="community" />
-        <CategoryLink category="community" />
-        <CategoryLink category="community" />
+      <div className={classes.categoryContainer}>
+        <CategoryLink category="community" imageURL={communityImageURL} />
+        <CategoryLink category="for sale" imageURL={saleImageURL} />
+        <CategoryLink category="jobs" imageURL={jobsImageURL} />
       </div>
     </div>
   );
